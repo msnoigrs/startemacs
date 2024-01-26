d04c70b780af513bcae6f1c01aad2c1db7b208a9
Masanao Igarashi <syoux2@gmail.com>
2024-01-26 17:06:05 +0900

    update PATH

--- myemacs.bat
+++ myemacs.bat
@@ -1,4 +1,4 @@
 @echo off
-set PATH=C:\emacs\bin;C:\msys64\usr\bin;C:\msys64\mingw64\bin;C:\Program Files\git\bin;%PATH%
+set PATH=C:\emacs\bin;C:\msys64\usr\local\bin;C:\msys64\usr\bin;C:\msys64\mingw64\bin;C:\Program Files\git\bin;%PATH%
 cd %USERPROFILE%
 c:\emacs\bin\runemacs.exe
