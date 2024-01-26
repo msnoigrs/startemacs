f83dcd10c4b15b3c7a70099344c450efffaad805
Masanao Igarashi <syoux2@gmail.com>
2024-01-26 17:07:08 +0900

    ExitCode

--- oiemacs.ps1
+++ oiemacs.ps1
@@ -4,6 +4,6 @@ $regedit = "$Env:SystemRoot\REGEDIT.exe"
 $regfile = $curdir + "\oiemacs.reg"
 $regeditp = Start-Process -FilePath $regedit -ArgumentList "/S /I $regfile" -Wait -PassThru
 
-if ($regeditp -ne 0) {
+if ($regeditp.ExitCode -ne 0) {
     "Fail"
 }
